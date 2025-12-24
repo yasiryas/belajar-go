1bcc5e52ca76dfbad57bd38849a99a49b4ac4568
Muhammad Yasir <yasir123983@gmail.com>
2025-12-24 11:47:18 +0700

    Feat: Pembelajaran For

--- if-else/main.go
+++ if-else/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func ifElse1() {
 
 	fmt.Print("Masukkan umur Anda: ")
 	var umur int
@@ -14,4 +14,26 @@ func main() {
 		fmt.Println("Anda sudah cukup umur.")
 	}
 
+}
+
+func ifElse2() {
+	fmt.Print("Berapa nilai kamu? ")
+	var nilai int
+	fmt.Scanln(&nilai)
+
+	if nilai >=90{
+		fmt.Println("Nilai Kamu A")
+	}else if nilai >=80{
+		fmt.Println("Nilai Kamu B")
+	}else if nilai >= 70{
+		fmt.Println("Nilai Kamu C")
+	}else if nilai<70 {
+		fmt.Println("Nilai Kamu D")
+	}
+
+}
+
+func main(){
+	ifElse1()
+	ifElse2()
 }
